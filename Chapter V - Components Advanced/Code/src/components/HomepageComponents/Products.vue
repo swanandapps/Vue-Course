26b2aa7822ff4a828ccee2b38cfe681c54b25cb9
Swanand Kadam <swanandkadam@Swanands-MacBook-Air.local>
2022-11-16 08:54:55 +0530

    till chap 5

---
+++ Chapter V - Components Advanced/Code/src/components/HomepageComponents/Products.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="products">
+    <div v-for="item in Products" :key="item" id="individual_product">
+      <img :src="item.imageUrl" alt="" />
+
+      <p>{{ item.title }}</p>
+
+      <p>{{ item.price }} Rs</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import data from "../../../data/products.json";
+
+const Products = ref(data);
+</script>
+
+<style scoped>
+#sort {
+  padding-top: 3%;
+  width: 15%;
+}
+#products {
+  display: grid;
+
+  grid-template-columns: repeat(4, 1fr);
+
+  grid-gap: 3%;
+  margin-top: 3%;
+}
+
+#products img {
+  width: 100%;
+}
+
+#individual_product * {
+  margin-top: 2%;
+}
+</style>
---
+++ Chapter V - Components Advanced/readme.md
@@ -0,0 +1,4 @@
+register Products Component
+setup hardcoded products data
+show the data using - Vue directive ( v-for )
+Reactivity API ( ref & reactive() )
